376e869d9e785e45015329837650583066b7e39d
CaptainHarrie <butchlinkle@gmail.com>
2025-02-18 11:35:20 +0000

    add browse route

--- nc-news/src/App.jsx
+++ nc-news/src/App.jsx
@@ -1,3 +1,4 @@
+import Browse from "./pages/Browse";
 import Landing from "./pages/Landing";
 import { Route, Routes } from "react-router";
 
@@ -6,6 +7,7 @@ function App() {
     <main className="bg-primary-50 px-10 py-5 mx-auto my-5 max-w-[1200px]">
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/browse" element={<Browse />} />
         <Route path="/*" element={"Not found"} />
       </Routes>
     </main>
