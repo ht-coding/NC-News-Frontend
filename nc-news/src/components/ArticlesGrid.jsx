c89676e5148b5f31d25c21c9a4e92a15ed7f3be1
CaptainHarrie <butchlinkle@gmail.com>
2025-02-18 11:55:48 +0000

    adjust breakpoint

--- nc-news/src/components/ArticlesGrid.jsx
+++ nc-news/src/components/ArticlesGrid.jsx
@@ -42,7 +42,7 @@ export default function ArticlesGrid({
   }, []);
   if (loading) return "loading...";
   return (
-    <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+    <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
       {articles.map((article, i) => (
         <Link
           to={"/article/" + article.article_id}
