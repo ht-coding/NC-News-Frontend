5c176def0d8645dedeef54f4fa57b7febd32c294
CaptainHarrie <butchlinkle@gmail.com>
2025-02-19 20:27:45 +0000

    reorganise files

--- nc-news/src/components/CommentsList.jsx
+++
@@ -1,9 +0,0 @@
-import Comment from "./Comment";
-
-export default function CommentsList({ comments }) {
-  if (comments.length === 0)
-    return "No comments have been left on this article yet.";
-  return comments.map((comment) => (
-    <Comment commentData={comment} key={comment.comment_id} />
-  ));
-}
