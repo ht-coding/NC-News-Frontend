e6d11b4353fdd901946482306b3c28c868eba01e
CaptainHarrie <butchlinkle@gmail.com>
2025-02-19 20:28:53 +0000

    extract article stats into their own component

---
+++ nc-news/src/components/article/Stats.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { Vote } from "../Vote";
+import { fetchComments } from "../../api";
+
+export default function Stats({ article_id, votes, author }) {
+  const [loading, setLoading] = useState(true);
+  const [comments, setComments] = useState([]);
+  useEffect(() => {
+    setLoading(true);
+    fetchComments(article_id).then((response) => {
+      setComments(response);
+      setLoading(false);
+    });
+  }, []);
+  if (loading) return null;
+  return (
+    <div className="mt-5 flex justify-center items-center relative">
+      {comments.length} Comments
+      <Vote
+        id={article_id}
+        votes={votes}
+        voteType={"articles"}
+        author={author}
+      ></Vote>
+    </div>
+  );
+}
