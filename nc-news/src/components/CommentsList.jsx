b72833bf9e7096c958bf550fd613e0fcde888a59
CaptainHarrie <butchlinkle@gmail.com>
2025-02-19 16:48:23 +0000

    move comment data up to the Article component, to allow for displaying the current comment count beneath articles

--- nc-news/src/components/CommentsList.jsx
+++ nc-news/src/components/CommentsList.jsx
@@ -1,19 +1,9 @@
-import { useEffect, useState } from "react";
-import { fetchComments } from "../api";
 import Comment from "./Comment";
 
-export default function CommentsList({ article_id }) {
-  const [comments, setComments] = useState([]);
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    setLoading(true);
-    fetchComments(article_id).then((response) => {
-      setComments(response);
-      setLoading(false);
-    });
-  }, []);
-  if (loading) return "loading...";
+export default function CommentsList({ comments }) {
   if (comments.length === 0)
     return "No comments have been left on this article yet.";
-  return comments.map((comment) => <Comment commentData={comment} />);
+  return comments.map((comment) => (
+    <Comment commentData={comment} key={comment.comment_id} />
+  ));
 }
