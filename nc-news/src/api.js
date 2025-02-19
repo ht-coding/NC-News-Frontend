bf1492e5a33e5e19b47b3df09b934fdb3616313e
CaptainHarrie <butchlinkle@gmail.com>
2025-02-19 11:36:42 +0000

    create fetchComments and fetchUserInfo api calls

--- nc-news/src/api.js
+++ nc-news/src/api.js
@@ -2,13 +2,25 @@ import axios from "axios";
 const baseURL = axios.create({ baseURL: "https://htpncnews.onrender.com/api" });
 
 export function fetchArticles(queries) {
-  return baseURL.get("/articles", { params: queries }).then((response) => {
-    return response.data.articles;
-  });
+  return baseURL
+    .get("/articles", { params: queries })
+    .then((response) => response.data.articles);
 }
 
 export function fetchSingleArticle(article_id) {
-  return baseURL.get("/articles/" + article_id).then((response) => {
-    return response.data.article;
-  });
+  return baseURL
+    .get("/articles/" + article_id)
+    .then((response) => response.data.article);
+}
+
+export function fetchComments(article_id) {
+  return baseURL
+    .get("/articles/" + article_id + "/comments")
+    .then((response) => response.data.comments);
+}
+
+export function fetchUserInfo(username) {
+  return baseURL
+    .get("/users/" + username)
+    .then((response) => response.data.user);
 }
