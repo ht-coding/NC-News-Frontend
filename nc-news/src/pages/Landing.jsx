a41a08657158686de7ef466a6117c22f934304be
CaptainHarrie <butchlinkle@gmail.com>
2025-02-19 12:26:44 +0000

    make html more semantic

--- nc-news/src/pages/Landing.jsx
+++ nc-news/src/pages/Landing.jsx
@@ -4,11 +4,14 @@ import ArticlesGrid from "../components/ArticlesGrid";
 export default function Landing() {
   return (
     <>
-      <h2 className="text-3xl mb-3">Top Articles</h2>
-      <ArticlesGrid sort_by="votes" limit="6"></ArticlesGrid>
-
-      <h2 className="text-3xl mb-3 mt-10">Recent Articles</h2>
-      <ArticlesGrid limit="6"></ArticlesGrid>
+      <section>
+        <h1 className="text-3xl mb-3">Top Articles</h1>
+        <ArticlesGrid sort_by="votes" limit="6"></ArticlesGrid>
+      </section>
+      <section>
+        <h1 className="text-3xl mb-3 mt-10">Recent Articles</h1>
+        <ArticlesGrid limit="6"></ArticlesGrid>
+      </section>
       <p className="text-center my-5">
         <Link
           className="text-primary-50 bg-primary-700 hover:bg-primary-800 px-5 py-3 rounded-2xl"
