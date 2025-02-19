3c481cb07dcc5d38bb154452ba406e5182931d99
CaptainHarrie <butchlinkle@gmail.com>
2025-02-19 20:31:18 +0000

    add new comment button and modal

---
+++ nc-news/src/components/comments/CommentsList.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import Comment from "./Comment";
+import { fetchComments } from "../../api";
+import CommentModal from "./CommentModal";
+import Loader from "../Loader";
+
+export default function CommentsList({ article_id }) {
+  const [newComment, setNewComment] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+  const [comments, setComments] = useState([]);
+  useEffect(() => {
+    setLoading(true);
+    fetchComments(article_id).then((response) => {
+      setComments(response);
+      setLoading(false);
+      setNewComment(null);
+    });
+  }, [newComment]);
+  if (loading) return <Loader />;
+  return (
+    <>
+      <div className="flex text-lg">
+        <button
+          className="text-primary-900 bg-secondary-300 hover:bg-secondary-200 cursor-pointer px-3 py-2 rounded-2xl border-0 mx-auto"
+          onClick={() => {
+            setShowModal(true);
+          }}
+        >
+          Post A Comment
+        </button>
+      </div>
+      {comments.length === 0
+        ? "No comments have been left on this article yet."
+        : comments.map((comment) => (
+            <Comment commentData={comment} key={comment.comment_id} />
+          ))}
+      {showModal && (
+        <CommentModal
+          setShowModal={setShowModal}
+          article_id={article_id}
+          setNewComment={setNewComment}
+        />
+      )}
+    </>
+  );
+}
