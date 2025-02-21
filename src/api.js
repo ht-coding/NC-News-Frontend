c62e6cb4f4b32cbdbab3b6ea837b185d5c6e9ad1
CaptainHarrie <butchlinkle@gmail.com>
2025-02-21 12:01:49 +0000

    create error handler function

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { handleErrors } from "./utils/handleErrors";
 const baseURL = axios.create({ baseURL: "https://htpncnews.onrender.com/api" });
 
 const apiKey = import.meta.env.VITE_PEXELS_API_KEY;
@@ -10,30 +11,46 @@ const pexelsURL = axios.create({
 export function fetchArticles(queries) {
   return baseURL
     .get("/articles", { params: queries })
-    .then((response) => response.data.articles);
+    .then((response) => response.data.articles)
+    .catch((error) => {
+      return handleErrors(error);
+    });
 }
 
 export function fetchCategories() {
-  return baseURL.get("/topics").then((response) => response.data.topics);
+  return baseURL
+    .get("/topics")
+    .then((response) => response.data.topics)
+    .catch((error) => {
+      return handleErrors(error);
+    });
 }
 
 export function fetchPhotoData(photoURL) {
   const regex = /(?<=photos\/)\d+/;
   const photo_id = photoURL.match(regex)[0];
-  return pexelsURL.get(photo_id).then((response) => {
-    return {
-      alt: response.data.alt,
-      photo_url: response.data.url,
-      photographer: response.data.photographer,
-      photographer_url: response.data.photographer_url,
-    };
-  });
+  return pexelsURL
+    .get(photo_id)
+    .then((response) => {
+      return {
+        alt: response.data.alt,
+        photo_url: response.data.url,
+        photographer: response.data.photographer,
+        photographer_url: response.data.photographer_url,
+      };
+    })
+    .catch((error) => {
+      return handleErrors(error);
+    });
 }
 
 export function fetchSingleArticle(article_id) {
   return baseURL
     .get("/articles/" + article_id)
-    .then((response) => response.data.article);
+    .then((response) => response.data.article)
+    .catch((error) => {
+      return handleErrors(error);
+    });
 }
 
 export function fetchComments(article_id) {
@@ -45,7 +62,10 @@ export function fetchComments(article_id) {
 export function fetchUserInfo(username) {
   return baseURL
     .get("/users/" + username)
-    .then((response) => response.data.user);
+    .then((response) => response.data.user)
+    .catch((error) => {
+      return handleErrors(error);
+    });
 }
 
 export function patchVote(endpoint, id, positive) {
@@ -53,19 +73,21 @@ export function patchVote(endpoint, id, positive) {
   return baseURL
     .patch(`/${endpoint}/${id}`, { inc_votes: vote })
     .then((response) => response.data.comment)
-    .catch((error) => Promise.reject(error));
+    .catch((error) => {
+      return handleErrors(error);
+    });
 }
 
 export function postComment(article_id, author, body) {
   return baseURL
     .post(`/articles/${article_id}/comments`, { body, author })
     .catch((error) => {
-      console.log(error);
-
-      return Promise.reject(error);
+      return handleErrors(error);
     });
 }
 
 export function deleteComment(comment_id) {
-  return baseURL.delete("/comments/" + comment_id);
+  return baseURL.delete("/comments/" + comment_id).catch((error) => {
+    return handleErrors(error);
+  });
 }
