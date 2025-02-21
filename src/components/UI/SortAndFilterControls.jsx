9b7568bf48ee556414f5cf1bc780818848573116
CaptainHarrie <butchlinkle@gmail.com>
2025-02-21 15:29:16 +0000

    extract controls into their own component

---
+++ src/components/UI/SortAndFilterControls.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router";
+import CategorySelect from "./CategorySelect";
+import OrderControls from "./OrderControls";
+import { ArrowLeft, LineVertical } from "@phosphor-icons/react";
+export default function SortAndFilterControls({
+  setGridSort,
+  setGridDescending,
+  category,
+}) {
+  return (
+    <>
+      {!category ? (
+        <CategorySelect />
+      ) : (
+        <Link
+          className="inline-flex items-center gap-1 bg-primary-200 hover:bg-primary-100 px-3 py-1 rounded"
+          to="/browse"
+        >
+          <ArrowLeft className="inline-block" /> All categories
+        </Link>
+      )}
+      <OrderControls
+        setGridSort={setGridSort}
+        setGridDescending={setGridDescending}
+      />
+      <LineVertical />
+    </>
+  );
+}
