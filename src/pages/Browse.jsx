6a82c8d9327e0ccdef79d61f8afe9e82fcb645cf
CaptainHarrie <butchlinkle@gmail.com>
2025-02-21 15:26:23 +0000

    update logic to allow dynamic sorting

--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -1,7 +1,8 @@
 import { useNavigate, useParams } from "react-router";
 import ArticlesGrid from "../components/articles/ArticlesGrid";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import StickyCollapseBar from "../components/UI/StickyCollapseBar";
+import CategoryDescription from "../components/CategoryDescription";
 
 export default function Browse() {
   useEffect(() => {
@@ -9,10 +10,22 @@ export default function Browse() {
   }, []);
   const { category } = useParams();
   const navigate = useNavigate();
+  const [gridSort, setGridSort] = useState("created_at");
+  const [gridDescending, setGridDescending] = useState(true);
   return (
     <>
-      <StickyCollapseBar category={category} />
-      <ArticlesGrid limit="0" category={category}></ArticlesGrid>
+      <StickyCollapseBar
+        setGridDescending={setGridDescending}
+        setGridSort={setGridSort}
+        category={category}
+      />
+      {category ? <CategoryDescription category={category} /> : null}
+      <ArticlesGrid
+        limit="0"
+        category={category}
+        sort_by={gridSort}
+        descending={gridDescending}
+      ></ArticlesGrid>
       <p className="text-center my-5">
         <a
           className="text-primary-900 bg-primary-300 hover:bg-primary-500 hover:text-primary-50 px-5 py-2 rounded-2xl mx-auto cursor-pointer"
